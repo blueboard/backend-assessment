ca46f6bc6638933ba96fb45b7f71448a617d3ad0
Adooylabs <amvelasco@gmail.com>
2015-08-28 11:35:10 -0700

    initialize rails app

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    scope module: :v1, contraints: ApiConstraints.new(version:1, default: :true) do
+      get '/', to: "data#index"
+      get 'show/:id', to: "data#show"
+      get 'create(:format)', to: "data#create"
+      delete 'delete/:id', to: "data#destroy"
+    end
+  end
+end
